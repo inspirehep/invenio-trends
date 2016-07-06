e5eaa4c26fa4bb31ec70a6d9b2edbb297ec2084a
Teo Stocco <teo.stocco@epfl.ch>
2016-07-06 14:08:01 +0200

    add js files from holding pen as a model

---
+++ invenio_trends/static/js/js/invenio_trends_ui/holdingpen/holdingpen.controllers.js
@@ -0,0 +1,131 @@
+/* This file is part of inspirehep.
+ * Copyright (C) 2016 CERN.
+ *
+ * inspirehep is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * inspirehep is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with inspirehep; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307, USA.
+ *
+ * In applying this license, CERN does not
+ * waive the privileges and immunities granted to it by virtue of its status
+ * as an Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+
+(function (angular) {
+
+  function HoldingPenSelectionCtrl($scope, Hotkeys, HoldingPenRecordService) {
+
+    $scope.vm.selected_records = [];
+    $scope.vm.selected_record_decisions = {};
+
+    $scope.toggleSelection = toggleSelection;
+    $scope.toggleAll = toggleAll;
+    $scope.isChecked = isChecked;
+    $scope.allChecked = allChecked;
+    $scope.setDecision = setDecision;
+
+    function getItemIdx(id) {
+      return $scope.vm.selected_records.indexOf(+id);
+    }
+
+    function toggleSelection(record) {
+      var _data_type = record._source._workflow.data_type;
+
+      if (isChecked(record._id)) {
+        $scope.vm.selected_records.splice(getItemIdx(+record._id), 1);
+        if (_data_type in $scope.vm.selected_record_decisions) {
+          $scope.vm.selected_record_decisions[_data_type]
+            .splice($scope.vm.selected_record_decisions[_data_type]
+              .indexOf(+record._id), 1);
+        }
+      }
+      else {
+        $scope.vm.selected_records.push(+record._id);
+        if (_data_type) {
+          if (!(_data_type in $scope.vm.selected_record_decisions)) {
+            $scope.vm.selected_record_decisions[_data_type] = [];
+          }
+          if(record._source._extra_data === undefined ||
+            record._source._extra_data.user_action == undefined)
+            $scope.vm.selected_record_decisions[_data_type].push(+record._id);
+        }
+      }
+    }
+
+    function setDecision(id, decision) {
+      HoldingPenRecordService.setBatchDecision(
+        $scope.vm.invenioSearchResults.hits.hits, [+id], decision)
+    }
+
+    function toggleAll() {
+
+      var remove_all = allChecked();
+      if (remove_all) {
+        $scope.vm.selected_records = [];
+        $scope.vm.selected_record_decisions = {};
+      } else {
+        angular.forEach($scope.$parent.vm.invenioSearchResults.hits.hits,
+          function (record) {
+            if (!isChecked(record._id)) {
+              $scope.vm.selected_records.push(+record._id);
+              if (record._source._workflow.data_type) {
+                if (!(record._source._workflow.data_type
+                  in $scope.vm.selected_record_decisions)) {
+                  $scope.vm.selected_record_decisions[
+                    record._source._workflow.data_type] = [];
+                }
+
+                if(record._source._extra_data === undefined || record._source._extra_data.user_action == undefined)
+                  $scope.vm.selected_record_decisions[record._source._workflow.data_type]
+                    .push(record._id);
+              }
+            }
+          });
+      }
+    }
+
+    function isChecked(id) {
+      return getItemIdx(+id) !== -1;
+    }
+
+    function allChecked() {
+      if (!$scope.$parent.vm.invenioSearchResults.hits) {
+        return false;
+      }
+      return $scope.vm.selected_records.length ===
+        $scope.$parent.vm.invenioSearchResults.hits.hits.length;
+    }
+
+    function reset() {
+      $scope.vm.selected_records = [];
+    }
+
+    var hotkey = Hotkeys.createHotkey({
+      key: 'ctrl+a',
+      callback: function () {
+        toggleAll();
+      }
+    });
+
+    Hotkeys.registerHotkey(hotkey);
+
+    $scope.$on('invenio.search.success', reset);
+  }
+
+  HoldingPenSelectionCtrl.$inject = ['$scope', 'Hotkeys', 'HoldingPenRecordService'];
+
+  angular.module('holdingpen.controllers', [])
+    .controller('HoldingPenSelectionCtrl', HoldingPenSelectionCtrl);
+
+})(angular);
