f74a0f69c5e4921c26f899f9871280f2a1c22939
Teo Stocco <teo.stocco@epfl.ch>
2016-08-16 15:19:01 +0200

    add basic config

--- invenio_trends/config.py
+++ invenio_trends/config.py
@@ -2,5 +2,12 @@ TRENDS_ENDPOINT = "api/trends"
 
 TRENDS_SOURCE = {
     'index': 'records-hep',
-    'doc': 'hep'
+    'origin_doc_type': 'hep',
+    'analysis_doc_type': 'trends-analysis',
+    'analysis_field': [
+        'abstracts.value'
+    ],
+    'date_field': 'earliest_date',
+    'minimum_date': '2013-02-01',
+    'maximum_date': None
 }
